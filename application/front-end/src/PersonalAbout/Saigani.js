5f2fdf4f366e63f83917968fd95e8a0ea13c9a8c
Sabur Saigani <Sabursaigani>
2022-06-19 18:19:59 -0700

    sabur is done

--- application/front-end/src/PersonalAbout/Saigani.js
+++ application/front-end/src/PersonalAbout/Saigani.js
@@ -3,7 +3,7 @@ import React from "react";
 function Saigani() {
     return (
       <div>
-        <img class= "Saigani_About_Image" src={require('../Images/Saigani_Image.png')} />
+        <img class="Saigani_About_Image" src={require('../Images/Saigani_Image.png') }  />
         <h1 className="Saigani_About_Name"> Sabur Saigani</h1>
         <p className="About_Me_Paragraph">Hello, My name is Sabur and my major is Computer Scienceat SFSU. This is my last summer class
        and I have to do another semester before i graduate. 
