6e313f9139ab11e3c33549d18851586861f40372
KshitizSareen <kshitizsareen709@gmail.com>
2022-06-30 20:00:07 -0700

    Added Search News API

--- application/backend/index.js
+++ application/backend/index.js
@@ -1,32 +1,6 @@
-
-
-const mysql = require('mysql');
-
-const con = mysql.createConnection({
-  host: "34.168.15.4",
-  user: "root",
-  password: "Ks@1234567890",
-  database: "YourSportsDB",
-  port: "3306"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-
-});
-
-query= `SELECT * FROM RegisteredUser`;
-con.query(query, (error, results, fields) => {
-  if (error) {
-    return console.error(error.message);
-  }
-  console.log(results);
-});
-
-
 const express = require("express");
 const router = require('./API/router');
+const searchNewsRouter = require("./API/SearchNewsAPI");
 
 const app = express();
 
@@ -35,5 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/",router);
+
+app.use('/api/searchnews/',searchNewsRouter);
 
 app.listen(8080, () => console.log('Server Begins'));
