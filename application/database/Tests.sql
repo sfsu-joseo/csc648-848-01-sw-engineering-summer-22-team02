95b8114151682b7b9d7da52e1780be725065c246
KshitizSareen <kshitizsareen709@gmail.com>
2022-06-30 21:49:34 -0700

    Made changes to Database

--- application/database/Tests.sql
+++ application/database/Tests.sql
@@ -1,3 +1,5 @@
 Use Yoursports;
 
 SELECT * FROM Article;
+
+SELECT image_URL,heading,subHeading,posttime,name as `Author`,sport FROM Article JOIN RegisteredUser ON user_id = Article.fk_authorid ORDER BY posttime DESC;
