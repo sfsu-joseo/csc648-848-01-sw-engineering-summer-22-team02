e204b1730683216d8d23206540861e5a0970cc10
KshitizSareen <kshitizsareen709@gmail.com>
2022-07-01 02:17:19 -0700

    Built Search API Complete

--- application/backend/API/SearchNewsAPI.js
+++ application/backend/API/SearchNewsAPI.js
@@ -4,10 +4,13 @@ const con = require("../conn");
 const searchNewsRouter = express.Router();
 
 searchNewsRouter.get("/search", (req, res) => {
-    let date = req.body.date;
-    let sport = req.body.sport;
+    let date = req.body.date == "" || req.body.date == null ? null : req.body.date.trim().toLowerCase();
+    let sport = req.body.sport == "" || req.body.sport == null ? null : req.body.sport.trim().toLowerCase();
+    let searchText = req.body.searchText == "" || req.body.searchText == null ? null : req.body.searchText.trim().toLowerCase();
     let query = "";
 
+    if (searchText==null)
+    {
     if (date!=null && sport!=null)
     {
         query = 'SELECT image_URL,heading,subHeading,posttime,name as `Author`,sport FROM (SELECT image_URL,heading,subHeading,fk_authorid,posttime,sport FROM Article WHERE sport = "'+sport+'" AND posttime >= "'+date+'") AS Articles JOIN RegisteredUser ON user_id = Articles.fk_authorid ORDER BY posttime DESC;';
@@ -24,6 +27,13 @@ searchNewsRouter.get("/search", (req, res) => {
 
         query = 'SELECT image_URL,heading,subHeading,posttime,name as `Author`,sport FROM Article JOIN RegisteredUser ON user_id = Article.fk_authorid ORDER BY posttime DESC;'
     }
+
+    }
+    else
+    {
+        query = 'SELECT * FROM (SELECT image_URL,heading,subHeading,posttime,sport,fk_authorid FROM Article WHERE heading LIKE "%'+searchText+'%") AS Articles JOIN RegisteredUser ON Articles.fk_authorid = RegisteredUser.user_id ORDER BY posttime DESC;'
+    }
+
 
     con.query(query, (error, results, fields) => {
         if (error) {
