bdff18955bea404118d5b726e47dae7bac50aaf8
KshitizSareen <kshitizsareen709@gmail.com>
2022-07-01 01:40:02 -0700

    Added Tags to Articles

---
+++ application/database/InsertArticleTags.sql
@@ -0,0 +1,61 @@
+USE Yoursports;
+
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (13,1);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (14,1);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (15,1);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (16,1);
+
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (1,2);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (5,2);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (17,2);
+
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (18,3);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (19,3);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (20,3);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (21,3);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (22,3);
+
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (21,4);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (22,4);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (23,4);
+
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (24,5);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (25,5);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (26,5);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (3,5);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (7,5);
+
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (24,6);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (25,6);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (26,6);
+
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (11,7);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (12,7);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (1,7);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (5,7);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (9,7);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (10,7);
+
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (11,8);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (12,8);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (1,8);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (5,8);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (9,8);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (10,8);
+
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (27,9);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (17,9);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (1,9);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (5,9);
+
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (11,10);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (12,10);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (28,10);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (29,10);
+
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (1,11);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (5,11);
+INSERT INTO Article_Tag (fk_TagId,fk_ArticleId) VALUES (30,11);
+
+
+
