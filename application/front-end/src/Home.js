951ffe99f822bb4ee19c529ba0fb60edaf168c53
Kevin Islas <kevin.islas04@gmail.com>
2022-06-30 19:02:52 -0700

    Simple navbar added to home page

--- application/front-end/src/Home.js
+++ application/front-end/src/Home.js
@@ -1,62 +1,22 @@
 import React from "react";
 import "./Home.css";
-import { Link } from "react-router-dom";
+import "./Navbar.css";
+
 
 const Home = () => {
   return (
     <>
-      <div className="header">
-        <h1> Software Engineering Class SFSU </h1>
-        <h1>Summer 2022 </h1>
-        <h1> Section 01 </h1>
-        <h1> Team 2 </h1>
-      </div>
-      <div className="buttons">
-        <h3>
-          <a className="button-link" href="/">
-            Kshitiz Sareen Test
-          </a>
-        </h3>
-
-        <h3>
-          <a className="button-link" href="/PersonalAbout/Islas">
-            Kevin Islas
-          </a>
-        </h3>
-
-        <h3>
-          <a className="button-link" href="/PersonalAbout/Ireland">
-            Shamar Ireland
-          </a>
-        </h3>
-
-        <h3>
-          <a className="button-link" href="/PersonalAbout/Saigani">
-            Sabur Saigani
-          </a>
-        </h3>
-
-        <h3>
-          <a className="button-link" href="/PersonalAbout/Guo">
-            Wenye Guo
-          </a>
-        </h3>
-
-        <h3>
-          <a className="button-link" href="/">
-            Mathew O Abiola
-          </a>
-        </h3>
-
-        <h3>
-          <a className="button-link" href="/PersonalAbout/Ip">
-            Jonathan Ip
-          </a>
-        </h3>
-      </div>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/About">About</a>
+        </li>
+      </ul>
     </>
-    // </div>
   );
 };
+// </div>
 
 export default Home;
