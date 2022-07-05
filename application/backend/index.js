aa57a870f2ce8851f481babd2c8258da66e7fe8d
KshitizSareen <kshitizsareen709@gmail.com>
2022-07-04 17:11:23 -0700

    Added Cors

--- application/backend/index.js
+++ application/backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = require('./API/router');
 const searchNewsRouter = require("./API/SearchNewsAPI");
+const cors = require('cors');
 
 const app = express();
 
@@ -11,5 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/",router);
 
 app.use('/api/searchnews/',searchNewsRouter);
+
+app.use(cors());
 
 app.listen(8080, () => console.log('Server Begins'));
