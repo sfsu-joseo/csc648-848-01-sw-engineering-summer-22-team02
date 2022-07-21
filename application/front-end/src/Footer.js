899586f01212e6e2895ef882f0bf908b57765f13
sireland@mail.sfsu.edu <sireland@mail.sfsu.edu>
2022-07-21 04:34:50 -0600

    Footer / Minor Updates

--- application/front-end/src/Footer.js
+++ application/front-end/src/Footer.js
@@ -5,23 +5,29 @@ import "./UploadArticle";
 import "./PlayerStats";
 
 const Footer = () => {
+
+  function redirect(){
+    alert("You are now leaving this Website");
+  }
   return (
     <>
       <ul className="footer">
         <div class="row">
           <div class="column">
-            <p href="/ArticleView">Terms of Service</p>
+            <br></br>
+            <a href="/ArticleView">Terms of Service</a>
           </div>
           <div class="column">
-            <p>Some text..</p>
-            <p>Some text..</p>
-            <p>Some text..</p>
+            <p>Contact Us</p>
+            <a href="https://www.facebook.com/sanfranciscostate" onClick={redirect}> <p>Facebook</p></a>
+            <a href="https://twitter.com/SFSU?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" onClick={redirect}> <p>Twitter</p></a>
+            <a href="https://www.instagram.com/sanfranciscostate/?hl=en" onClick={redirect}> <p>Instagram</p></a>
           </div>
           <div class="column">
-            <p>Some text..</p>
-            <p>Some text..</p>
-            <p>Some text..</p>
-            <p>Some text..</p>
+            <p>Address</p>
+            <p>San Francisco State University</p>
+            <p>San Francisco</p>
+            <p>California</p>
           </div>
         </div>
       </ul>
