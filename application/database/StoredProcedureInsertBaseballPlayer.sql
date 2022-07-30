ab427f17ae83b2acc5705c00ae5f37575e7bde52
WenyeGuo <wenyeguo@outlook.com>
2022-07-30 02:29:29 -0700

    Add insert procedures

---
+++ application/database/StoredProcedureInsertBaseballPlayer.sql
@@ -0,0 +1,25 @@
+Use YourSportsDB;
+
+DELIMITER $$
+CREATE PROCEDURE `InsertBaseBallPlayer` (IN inBats DECIMAL, 
+IN inRuns DECIMAL,
+IN inHits DECIMAL,
+IN inSingles DECIMAL,
+IN inDoubles DECIMAL,
+IN inTriples DECIMAL,
+IN inHomeRuns DECIMAL,
+IN inRunsBatted DECIMAL,
+IN inBattingAverage DECIMAL,
+IN inOuts DECIMAL,
+IN inPlayerID INT,
+IN inplayerURL varchar(255)
+)
+
+BEGIN
+INSERT INTO `Baseball_Player`
+(`AtBats`, `Runs`, `Hits`, `Singles`, `Doubles`, `Triples`, `HomeRuns`, `RunsBatted`, `BattingAverage`, `Outs`, `fk_Player_ID`, `playerURL`)
+VALUES
+(inBats, inRuns, inHits, inSingles, inDoubles, inTriples, inHomeRuns, inRunsBatted, inBattingAverage, inOuts, inPlayerID, inplayerURL);
+
+END$$
+DELIMITER ;
