acc9b7c4e849acbe9eeef3bef789e83e452c1a3c
WenyeGuo <wenyeguo@outlook.com>
2022-07-30 21:26:22 -0700

    Add StoredProcedureCheckAccountLikeOrDislike

---
+++ application/database/StoredProcedureCheckAccountLikeOrDislike.sql
@@ -0,0 +1,41 @@
+Use YourSportsDB;
+
+DELIMITER $$
+CREATE PROCEDURE `CheckAccountLikeOrDislike`(IN inArticleID INT,
+IN inAccountID INT)
+
+BEGIN
+DECLARE count INT;
+DECLARE countLikes INT;
+DECLARE countDislikes INT;
+
+SET count = 0;
+
+SET countLikes = (SELECT count(*) FROM Article_Likes
+WHERE fk_Article_ID = inArticleID
+AND fk_Account_ID = inAccountID);
+
+SET countDislikes = (SELECT count(*) FROM Article_Dislikes
+WHERE fk_Article_ID = inArticleID
+AND fk_Account_ID = inAccountID);
+
+IF  countLikes = 1
+THEN 
+SET count = 1;
+END IF;
+
+IF countDislikes = 1
+THEN
+SET count = 2;
+END IF;
+
+SELECT count;
+
+
+
+
+
+
+
+END$$
+DELIMITER ;
